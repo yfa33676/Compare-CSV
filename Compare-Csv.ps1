defb4b896c2471e5331a8262aa401c81e0d72e21
yfa33676 <yfa33676@gmail.com>
2021-07-12 15:59:55 +0900

    CSV比較

---
+++ Compare-Csv.ps1
@@ -0,0 +1,68 @@
+# エンコーディング（SJIS）
+$OutputEncoding = [console]::OutputEncoding
+
+# ファイル保存ダイアログ
+Add-Type -AssemblyName System.Windows.Forms
+$SaveFileDialog = New-Object System.Windows.Forms.SaveFileDialog 
+$SaveFileDialog.Filter = "csvファイル(*.csv)|*.csv|すべてのファイル(*.*)|*.*"
+$SaveFileDialog.InitialDirectory = ".\"
+
+# ファイル開くダイアログ
+Add-Type -AssemblyName System.Windows.Forms
+$OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog 
+$OpenFileDialog.Filter = "csvファイル(*.csv)|*.csv|すべてのファイル(*.*)|*.*"
+$OpenFileDialog.InitialDirectory = ".\"
+
+Write-Progress -Activity "変更前CSVの読み込み" -Status 読み込み開始
+$OpenFileDialog.Filename = "変更前.csv"
+if($OpenFileDialog.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK){
+  $Header = (Get-Content -Path $OpenFileDialog.Filename)[0].Split(",") | % Replace "`"" "" | % Trim
+  Write-Progress -Activity "変更前CSVの読み込み" -Status 読み込み中
+  $A = Get-Content -Path $OpenFileDialog.Filename | % Insert 0 "削除,"
+  Write-Progress -Activity "変更前CSVの読み込み" -Status 読み込み終了
+  $A[0] = $A[0].Replace("削除", "変更区分")
+}
+
+Write-Progress -Activity "変更後CSVの読み込み" -Status 読み込み開始
+$OpenFileDialog.Filename = "変更後.csv"
+if($OpenFileDialog.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK){
+  Write-Progress -Activity "変更後CSVの読み込み" -Status 読み込み中
+  $B = Get-Content -Path $OpenFileDialog.Filename | % Insert 0 "追加,"
+  Write-Progress -Activity "変更後CSVの読み込み" -Status 読み込み終了
+  $B[0] = $B[0].Replace("追加", "非表示")
+}
+
+
+Write-Progress "CSVをソート"
+$SortKey  = $Header | Out-GridView -PassThru -Title "ソートキーを選んでください"
+$C = $A + $B | ConvertFrom-CSV | Sort-Object ($SortKey + "変更区分")
+
+Write-Progress "CSVを比較" -percentComplete 0
+$CompKey = $Header | Out-GridView -PassThru -Title "比較キーを選んでください"
+for($i = 0; $i -lt $C.length - 1; $i++){
+  Write-Progress "CSVを比較" -percentComplete (100 * ($i)/($C.length))
+  if($C[$i].変更区分 -eq "削除" -and $C[$i+1].変更区分 -eq "追加"){
+    if(
+      ($C[$i]   | Select-Object $CompKey | ConvertTo-Json) -eq
+      ($C[$i+1] | Select-Object $CompKey | ConvertTo-Json)
+    ){
+      $C[$i].変更区分 = "非表示"
+      $C[$i+1].変更区分 = ""
+    } elseif(
+      ($C[$i]   | Select-Object $SortKey | ConvertTo-Json) -eq
+      ($C[$i+1] | Select-Object $SortKey | ConvertTo-Json)
+    ){
+      $C[$i].変更区分 = "変更前"
+      $C[$i+1].変更区分 = "変更後"
+    }
+    $i++
+  }
+}
+
+$C = $C | ? 変更区分 -ne "非表示"
+$C | Out-GridView -title "比較結果" -PassThru | Out-Null
+
+$SaveFileDialog.Filename = "比較結果.csv"
+if ($SaveFileDialog.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK){
+  $C | Export-Csv -Encoding Default -NoTypeInformation -Path $SaveFileDialog.Filename
+}
